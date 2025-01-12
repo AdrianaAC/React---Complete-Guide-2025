553c15028f26aa915703b7347a780d9df5690867
Adriana Alves <adrianaalves098@gmail.com>
2025-01-12 03:38:14 +0000

    42 final

--- 03 React Essentials/ReactEssentialsPage/src/components/Header.jsx
+++ 03 React Essentials/ReactEssentialsPage/src/components/Header.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 export default function Header() {
+  const reactDesc = ["Fundamental", "Crucial", "Core", "Essential"];
+
+  function genRandomIndex(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+
+  let randomDesc = reactDesc[genRandomIndex(reactDesc)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {randomDesc} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
