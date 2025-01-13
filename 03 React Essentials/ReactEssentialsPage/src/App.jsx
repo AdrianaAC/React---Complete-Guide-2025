5658a9c76d377b089cd1d184fd74e4239a26b7e3
Adriana Alves <adrianaalves098@gmail.com>
2025-01-13 00:19:19 +0000

    52 final

--- 03 React Essentials/ReactEssentialsPage/src/App.jsx
+++ 03 React Essentials/ReactEssentialsPage/src/App.jsx
@@ -5,8 +5,15 @@ import TabButton from "./components/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data/coreConcepts.js";
 
 function App() {
-  function handleClick(event) {
-    console.log(event.target.textContent);
+  let dynamic = "Dynamic Text";
+  // function handleClick(event) {
+  //   console.log(event.target.textContent);
+  //   dynamic = event.target.textContent;
+  //   return dynamic;
+  // }
+
+  function handleClick(clickedButton) {
+    console.log(clickedButton);
   }
 
   return (
@@ -29,11 +36,14 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton clicking={handleClick}>Components</TabButton>
-            <TabButton clicking={handleClick}>JSX</TabButton>
-            <TabButton clicking={handleClick}>Props</TabButton>
-            <TabButton clicking={handleClick}>State</TabButton>
+            <TabButton clicking={() => handleClick("Components")}>
+              Components
+            </TabButton>
+            <TabButton clicking={() => handleClick("JSX")}>JSX</TabButton>
+            <TabButton clicking={() => handleClick("Propss")}>Props</TabButton>
+            <TabButton clicking={() => handleClick("State")}>State</TabButton>
           </menu>
+          {dynamic}
         </section>
       </main>
     </div>
