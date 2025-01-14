fc9dbfbd905c995091663d130fcbcc7a91bd2b7c
Adriana Alves <adrianaalves098@gmail.com>
2025-01-14 00:27:37 +0000

    55 final

--- 03 React Essentials/ReactEssentialsPage/src/App.jsx
+++ 03 React Essentials/ReactEssentialsPage/src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data/dynamicData.js";
 import { CORE_CONCEPTS } from "./data/coreConcepts.js";
 
 function App() {
-  let dynamic = "Dynamic Text";
+  const [selectedData, setSelectedData] = useState("Components");
+
   // function handleClick(event) {
   //   console.log(event.target.textContent);
   //   dynamic = event.target.textContent;
@@ -14,6 +16,7 @@ function App() {
 
   function handleClick(clickedButton) {
     console.log(clickedButton);
+    setSelectedData(clickedButton);
   }
 
   return (
@@ -40,10 +43,18 @@ function App() {
               Components
             </TabButton>
             <TabButton clicking={() => handleClick("JSX")}>JSX</TabButton>
-            <TabButton clicking={() => handleClick("Propss")}>Props</TabButton>
+            <TabButton clicking={() => handleClick("Props")}>Props</TabButton>
             <TabButton clicking={() => handleClick("State")}>State</TabButton>
           </menu>
-          {dynamic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedData].title}</h3>
+            <p>{EXAMPLES[selectedData].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedData].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
