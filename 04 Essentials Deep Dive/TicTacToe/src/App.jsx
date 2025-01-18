9dab8811e433df59671b1b38a8c4e20cc22be4d6
Adriana Alves <adrianaalves098@gmail.com>
2025-01-18 03:21:26 +0000

    73. Added structure +  player dashboard

--- 04 Essentials Deep Dive/TicTacToe/src/App.jsx
+++ 04 Essentials Deep Dive/TicTacToe/src/App.jsx
@@ -1,6 +1,27 @@
 function App() {
   return (
- <p>Coming soon!</p>
+ <main>
+  <div className="gameDashboard">
+    <ol className="playersDashboard">
+      <li>
+        <span className="player">
+        <span className="playerName">Player 1</span>
+        <span className="playerSymbol">X</span>
+        </span>
+        <button>Edit</button>
+      </li>
+      <li>
+        <span className="player">
+      <span className="playerName">Player 2</span>
+      <span className="playerSymbol">O</span>
+      </span>
+      <button>Edit</button>
+      </li>
+    </ol>
+    GAME BOARD
+  </div>
+  LOG
+  </main>
   );
 }
 
