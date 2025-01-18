543d9b60d70119b62f74477c31ca30367df90689
Adriana Alves <adrianaalves098@gmail.com>
2025-01-18 04:43:13 +0000

    75. useState for <Player/>

--- 04 Essentials Deep Dive/TicTacToe/src/components/Player.jsx
+++ 04 Essentials Deep Dive/TicTacToe/src/components/Player.jsx
@@ -1,18 +1,33 @@
-import PropTypes from "prop-types";
-
-Player.propTypes = {
-  playerName: PropTypes.string.isRequired,
-  playerSymbol: PropTypes.string.isRequired,
-};
+import React, { useState } from "react";
 
 export default function Player({ playerName, playerSymbol }) {
+  const [name, setName] = useState(playerName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function editName() {
+    setIsEditing(!isEditing);
+  }
+
+  function handleInputName(event) {
+    setName(event.target.value);
+  }
+
+ let nameRendering;
+
+    if (isEditing) {
+    nameRendering = (<input type="text" onChange={handleInputName} required placeholder={name}/>);
+    } else {
+    nameRendering = (<span className="playerName">{name}</span>);
+    }
+
+
   return (
     <li>
       <span className="player">
-        <span className="playerName">{playerName}</span>
+        {nameRendering}
         <span className="playerSymbol">{playerSymbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={editName}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
