f7305c4b0bdd1d6056c304e440abc7e513631483
Adriana Alves <adrianaalves098@gmail.com>
2025-01-19 21:36:46 +0000

    81. Updated gameboard state immutably

--- 04 Essentials Deep Dive/TicTacToe/src/components/GameBoard.jsx
+++ 04 Essentials Deep Dive/TicTacToe/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,20 +7,32 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
-  return <ol className="gameBoard">
-    {
-        initialGameBoard.map((row, rowIndex)=>
-            <li key={rowIndex}>
-                <ol>
-                    {row.map((playerSymbol, colIndex)=>
-                        <li key={colIndex}>
-                            <button>{playerSymbol}</button>
-                        </li>
-                    )}
-                </ol>
-            </li>
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-        )
-    }
-  </ol>;
+  function handleSquareClick(rowIndex, colIndex) {
+    setGameBoard((prevGB) => {
+      const updatedBoard = [...prevGB.map((row) => [...row])];
+      updatedBoard[rowIndex][colIndex] = "X";
+      console.log(updatedBoard);
+      return updatedBoard;
+    });
+  }
+
+  return (
+    <ol className="gameBoard">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSquareClick(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
