a8939efb04c909a99d6bd7207821388ca260c03f
Adriana Alves <adrianaalves098@gmail.com>
2025-01-21 01:16:40 +0000

    89. Outsorcing winning combinations from a js file

--- 04 Essentials Deep Dive/TicTacToe/src/App.jsx
+++ 04 Essentials Deep Dive/TicTacToe/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./data/winningCombos";
 
 function App() {
   const [activePlayer, setActivePlayer] = useState("X");
