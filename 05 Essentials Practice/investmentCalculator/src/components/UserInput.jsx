fa1c0faaf2802c1841994339d34d98dd81001b06
Adriana Alves <adrianaalves098@gmail.com>
2025-02-04 15:06:45 +0000

    99. Added <UserInput/> component

---
+++ 05 Essentials Practice/investmentCalculator/src/components/UserInput.jsx
@@ -0,0 +1,22 @@
+export default function UserInput() {
+  return (
+    <div className="inputGroup">
+      <p>
+        <label>Initial Investment</label>
+        <input type="number" required />
+      </p>
+      <p>
+        <label>Annual Investment</label>
+        <input type="number" required />
+      </p>
+      <p>
+        <label>Expected Return</label>
+        <input type="number" required />
+      </p>
+      <p>
+        <label>Duration</label>
+        <input type="number" required />
+      </p>
+    </div>
+  );
+}
