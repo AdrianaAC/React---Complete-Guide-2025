4fbf5cfd1956c6c870e10d3c6c2a650c8064220f
Adriana Alves <adrianaalves098@gmail.com>
2025-02-15 02:05:46 +0000

    103. Outputting Results in a <table>

--- 05 Essentials Practice/investmentCalculator/src/components/Results.jsx
+++ 05 Essentials Practice/investmentCalculator/src/components/Results.jsx
@@ -1,17 +1,45 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Results({ userInput }) {
   let results = calculateInvestmentResults(userInput);
+  const initialInvestment =
+    results[0].valueEndOfYear -
+    results[0].interest -
+    results[0].annualInvestment;
+
   console.log(results);
   return (
     <section className="results">
-      <h2>Results</h2>
-      <p>
-        You will invest <strong>${userInput.initialInvestment}</strong>{" "}
-        initially and After {userInput.duration} years, your investment will be
-        worth <strong>${userInput.expectedReturn}</strong>.
-        <p>You will receive:</p>
-      </p>
+      <table className="resultsTable">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Interest (Total)</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((result) => {
+            console.log(result);
+            const totalInterest =
+              result.valueEndOfYear -
+              result.annualInvestment * result.year -
+              initialInvestment;
+            const totalInvestedCapital = result.valueEndOfYear - totalInterest;
+            return (
+              <tr key={result.year}>
+                <td>{result.year}</td>
+                <td>{formatter.format(result.valueEndOfYear)}</td>
+                <td>{formatter.format(result.interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totalInvestedCapital)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </section>
   );
 }
