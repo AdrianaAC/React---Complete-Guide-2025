0e81c84405d47f2bd8c1d4e3b66943b63f249a55
Adriana Alves <adrianaalves098@gmail.com>
2025-02-22 05:27:45 +0000

    114. Creating flexible components with styled-components

--- 07 Styling/ReactArt/src/components/AuthInputs.jsx
+++ 07 Styling/ReactArt/src/components/AuthInputs.jsx
@@ -8,6 +8,27 @@ const StyledControls = styled.div`
   gap: 0.5rem;
   margin-bottom: 1.5rem;
 `;
+
+const StyledLabel = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: #6b7280;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: #374151;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -30,29 +51,28 @@ export default function AuthInputs() {
 
   return (
     <div className={styles.authInputs}>
-      {/* <div className={styles.controls}> */}
       <StyledControls>
         <p>
-          <label
-            className={`${styles.label} ${emailNotValid ? styles.invalid : ""}`}
+          <StyledLabel
+            className={emailNotValid ? styles.invalid : ""}
           >
             Email
-          </label>
-          <input
+          </StyledLabel>
+          <StyledInput
             type="email"
             className={emailNotValid ? styles.invalid : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label
+          <StyledLabel
             className={`${styles.label} ${
               passwordNotValid ? styles.invalid : ""
             }`}
           >
             Password
-          </label>
-          <input
+          </StyledLabel>
+          <StyledInput
             type="password"
             className={passwordNotValid ? styles.invalid : undefined}
             onChange={(event) =>
