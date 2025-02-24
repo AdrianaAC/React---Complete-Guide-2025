36cbb294ad8df00b14218a0e329190acb2b7ae3f
Adriana Alves <adrianaalves098@gmail.com>
2025-02-24 01:53:20 +0000

    cleaning

--- 07 Styling/ReactArt/src/components/Header.jsx
+++ 07 Styling/ReactArt/src/components/Header.jsx
@@ -1,16 +1,15 @@
 import logo from "../assets/logo.png";
-// import "./Header.module.css";
-// import { styled } from "styled-components";
+
 
 export default function Header() {
   return (
     <header className="flex flex-col items-center mt-8 mb-16">
       <img
         src={logo}
-        className="mb-8 w-44 h-44 object-contain"
+        className="mb-8 w-44 h-54 object-contain"
         alt="A canvas"
       />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800">
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
         ReactArt
       </h1>
       <p>A community of artists and art-lovers.</p>
