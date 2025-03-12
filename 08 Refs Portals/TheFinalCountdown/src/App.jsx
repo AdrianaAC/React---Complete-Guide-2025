7611c52a6e90db357d40c8dd15811fd7297f5ea8
AdrianaAC <adrianaalves098@gmail.com>
2025-03-12 23:36:34 +0000

    137. Added <TimerChallenge/>

--- 08 Refs Portals/TheFinalCountdown/src/App.jsx
+++ 08 Refs Portals/TheFinalCountdown/src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Going easy?" time={1} />
+        <TimerChallenge title="Try a little harder" time={5} />
+        <TimerChallenge title="Are you insane?" time={17} />
+        <TimerChallenge title="Good luck on this one!!" time={29} />
+      </div>
     </>
   );
 }
